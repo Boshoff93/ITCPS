9cc19fb4533675c11937e5e3e47927c56435c53d
Wiehan <boshoff.2@wright.edu>
2017-02-08 17:02:46 -0500

    Scenario four complete untill advance display screen

--- app/src/main/java/com/example/wiehan/kate_app_final/Activity_Scenario_ScenFour.java
+++ app/src/main/java/com/example/wiehan/kate_app_final/Activity_Scenario_ScenFour.java
@@ -1,7 +1,10 @@
 package com.example.wiehan.kate_app_final;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity_Scenario_ScenFour extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class Activity_Scenario_ScenFour extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scenario_scen_four);
+
+        Button buttonContinue = (Button) findViewById(R.id.buttonContinue_Scen4);
+
+        buttonContinue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Advance_Display_ScenFour.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/example/wiehan/kate_app_final/Advance_Display_ScenFour.java
@@ -0,0 +1,13 @@
+package com.example.wiehan.kate_app_final;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Advance_Display_ScenFour extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_advance_display_scen_four);
+    }
+}

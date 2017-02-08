66f82f29f89e071c94b95253d2992df97ad6ef87
Wiehan <boshoff.2@wright.edu>
2017-02-08 17:23:11 -0500

    Scenario four update a little bit

--- app/src/main/java/com/example/wiehan/kate_app_final/Advance_Display_ScenFour.java
+++ app/src/main/java/com/example/wiehan/kate_app_final/Advance_Display_ScenFour.java
@@ -2,8 +2,27 @@ package com.example.wiehan.kate_app_final;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Advance_Display_ScenFour extends AppCompatActivity {
+    GraphView graphBPAdvance ;
+    GraphView graphPulseAdvance ;
+    GraphView graphRRAdvance;
+    GraphView graphTempAdvance;
+    TextView BPNumAdvance ;
+    TextView pulseNumAdvance ;
+    TextView RRNumAdvance ;
+    TextView tempNumAdvance ;
+    TextView textInfo ;
+    TextView textPatient1, textPatient2, textPatient3, textPatient4, textPatient5 ;
+    LineGraphSeries<DataPoint> seriesBP ;
+    LineGraphSeries<DataPoint> seriesPulse ;
+    LineGraphSeries<DataPoint> seriesRR ;
+    LineGraphSeries<DataPoint> seriesTemp ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
